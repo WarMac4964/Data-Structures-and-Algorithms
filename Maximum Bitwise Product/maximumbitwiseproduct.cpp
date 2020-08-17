ce6009258ac600ee8b681d44db180c416f59b86c
WarMac4964 <you@example.com>
2020-08-17 15:07:36 +0530

    Maximum Bitwise Product

--- Maximum Bitwise Product/maximumbitwiseproduct.cpp
+++ Maximum Bitwise Product/maximumbitwiseproduct.cpp
@@ -5,4 +5,26 @@ using std::cin;
 using std::cout;
 using std::vector;
 
-int MaxBitwiseProduct(const )
+int MaxBitwiseProduct(const vector<int>& numbers) {
+    int result = 0;
+    int n = numbers.size();
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(numbers[i]*numbers[j]>result){
+                result = numbers[i]*numbers[j];
+            }
+        }
+    }
+    return result;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin>>numbers[i];
+    }
+    int result = MaxBitwiseProduct(numbers);
+    cout<<result<<"\n";
+    return 0;
+}
